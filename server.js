c4a3e9bcc8ccee2e903202621d0cb32be2d15106
Jonny K <Kempj2.jk@gmail.com>
2020-10-29 16:55:36 -0700

    File structure and boilerplate code in place for application

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 4000;
+
+
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", { useNewUrlParser: true });
+
+//controller routes
+const apiRoutes = require('./controllers/apiController.js')
+app.use(apiRoutes)
+
+//handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main", }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
