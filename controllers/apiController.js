eb387f27776e3e13c8bb6812e0a8ce16b4cfb430
Jonny K <Kempj2.jk@gmail.com>
2020-10-29 18:19:38 -0700

    gets and add workout functing

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,19 +3,26 @@ const router = express.Router();
 const db = require("../models");
 
 
-router.get("/workouts", (req, res) => {
+router.get("/api/workouts", (req, res) => {
     db.Workout.find({}, (err, data) =>{
         if (err) throw err
         res.json(data)
     })
 });
 
-router.get("/drills", (req, res) => {
+router.get("/api/drills", (req, res) => {
     db.Drill.find({}, (err, data) =>{
         if (err) throw err
         res.json(data)
     })
 });
+
+router.post("/api/create/workout/:name", (req, res) =>{
+    db.Workout.create([{name:req.params.name}], (err, data)=>{
+        if (err) throw err
+        res.json(data)
+    })
+})
 
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,5 +3,4 @@
 module.exports = {
   Drill: require("./Drill"),
   Workout: require("./Workout")
-
 };
