1269ea60863767ac07606bb01ad5d80dbac90d19
Jonny K <Kempj2.jk@gmail.com>
2020-10-29 17:51:49 -0700

    api routes for get

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const db = require("../models");
 
 
-router.get("/", ({body}, res) => {
-    res.render("index")
+router.get("/workouts", (req, res) => {
+    db.Workout.find({}, (err, data) =>{
+        if (err) throw err
+        res.json(data)
+    })
+});
+
+router.get("/drills", (req, res) => {
+    db.Drill.find({}, (err, data) =>{
+        if (err) throw err
+        res.json(data)
+    })
 });
 
 
